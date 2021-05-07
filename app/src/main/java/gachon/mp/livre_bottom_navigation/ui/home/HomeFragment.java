d1257fc1f8c95cf0eba89caff10d5c816f5f364f
MinaRoh <53070295+MinaRoh@users.noreply.github.com>
2021-05-07 21:05:56 +0900

    add tree

--- app/src/main/java/gachon/mp/livre_bottom_navigation/ui/home/HomeFragment.java
+++ app/src/main/java/gachon/mp/livre_bottom_navigation/ui/home/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,6 +18,11 @@ import gachon.mp.livre_bottom_navigation.R;
 
 public class HomeFragment extends Fragment {
 
+    Button button;
+    ImageView bg;
+    ImageView trunk;
+    ImageView leaves;
+    int number = 0;
     private HomeViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -23,17 +30,66 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        final TextView textView = root.findViewById(R.id.text_dashboard);
 
 
+        return root;
+    }
 
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        button = getActivity().findViewById(R.id.button);
+        leaves = getActivity().findViewById(R.id.leaves);
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
+            public void onClick(View view) {
 
-                textView.setText(s);
+                if(number==11) number=0;
+                else number++;
+
+                switch (number) {
+                    case 1:
+                        leaves.setImageResource(R.drawable.level1);
+                        break;
+                    case 2:
+                        leaves.setImageResource(R.drawable.level2);
+                        break;
+                    case 3:
+                        leaves.setImageResource(R.drawable.level3);
+                        break;
+                    case 4:
+                        leaves.setImageResource(R.drawable.level4);
+                        break;
+                    case 5:
+                        leaves.setImageResource(R.drawable.level5);
+                        break;
+                    case 6:
+                        leaves.setImageResource(R.drawable.level6);
+                        break;
+                    case 7:
+                        leaves.setImageResource(R.drawable.level7);
+                        break;
+                    case 8:
+                        leaves.setImageResource(R.drawable.level8);
+                        break;
+                    case 9:
+                        leaves.setImageResource(R.drawable.level9);
+                        break;
+                    case 10:
+                        leaves.setImageResource(R.drawable.level10);
+                        break;
+                    case 11:
+                        leaves.setImageResource(R.drawable.level11);
+                        break;
+                    default: number=0; break;
+
+                }
             }
         });
-        return root;
+
+
     }
 }
