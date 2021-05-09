170010e62ca94206a4fdbff66b387b83e196593e
yedin123 <73570623+yedin123@users.noreply.github.com>
2021-05-09 23:30:32 +0900

    Update SignInActivity.java

--- app/src/main/java/gachon/mp/livre_bottom_navigation/SignInActivity.java
+++ app/src/main/java/gachon/mp/livre_bottom_navigation/SignInActivity.java
@@ -9,17 +9,28 @@ import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class SignInActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
     private static final String TAG = "SignInActivity";
     private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+    private GoogleApiClient googleApiClient;
+    private static final int RC_SIGN_IN = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +39,7 @@ public class SignInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_in);
         ImageButton sign_in_btn_back = (ImageButton)findViewById(R.id.sign_in_btn_back);
         ImageButton btn_login = (ImageButton)findViewById(R.id.btn_login);
+        ImageButton btn_google_login = (ImageButton)findViewById(R.id.btn_google_login);
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
         //왼쪽 상단의 뒤로가기 버튼을 눌렀을 때
@@ -43,6 +55,33 @@ public class SignInActivity extends AppCompatActivity {
             public void onClick(View v) { signIn();
             }
         });
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+        btn_google_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(intent,RC_SIGN_IN);
+            }
+        });
+    }
+    @Override// 구글 로그인 인증 요청 했을 때 값 받음
+    protected void onActivityResult(int requestCode, int resultCode,@Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_SIGN_IN){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if(result.isSuccess()){
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivityForResult(intent, Protocol.SIGN_IN_OK);
+                finish();
+            }
+        }
     }
 
     /*활동을 초기화할 때 사용자가 현재 로그인되어 있는지 확인*/
@@ -95,4 +134,8 @@ public class SignInActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
