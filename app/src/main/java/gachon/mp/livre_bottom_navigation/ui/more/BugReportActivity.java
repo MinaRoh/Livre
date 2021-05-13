64086c73487c3f56424220f3c477723a1169ac4d
ojm51 <ojm51@naver.com>
2021-05-14 00:58:34 +0900

    Fix BugReportActivity.java

--- app/src/main/java/gachon/mp/livre_bottom_navigation/ui/more/BugReportActivity.java
+++ app/src/main/java/gachon/mp/livre_bottom_navigation/ui/more/BugReportActivity.java
@@ -1,6 +1,9 @@
 package gachon.mp.livre_bottom_navigation.ui.more;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +15,19 @@ public class BugReportActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_more_6bug_report);
+
+        TextView email = (TextView) findViewById(R.id.textView);
+        email.setOnClickListener(new TextView.OnClickListener() {
+            public void onClick(View view) {
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.setType("plain/text");
+                String[] address = {"email@gmail.com"};
+                email.putExtra(Intent.EXTRA_EMAIL, address);
+                email.putExtra(Intent.EXTRA_SUBJECT, "test@test");
+                email.putExtra(Intent.EXTRA_TEXT, "발견하신 문제점 또는 불편하신 점을 적어주세요");
+                startActivity(email);
+            }
+        });
     }
 
 }
