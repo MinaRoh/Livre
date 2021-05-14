106aaaf0ad853ffc724e8f821851fe9ba663ae6f
Yumi Ko <kumi3994@gmail.com>
2021-05-14 16:43:55 +0900

    Add execption in PAsswordResetActivity
    해당 이메일 가입자가 없는 경우 예외처리

--- app/src/main/java/gachon/mp/livre_bottom_navigation/PasswordResetActivity.java
+++ app/src/main/java/gachon/mp/livre_bottom_navigation/PasswordResetActivity.java
@@ -13,8 +13,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 
-    public class PasswordResetActivity extends AppCompatActivity{
+public class PasswordResetActivity extends AppCompatActivity{
     private static final String TAG = "PasswordResetActivity";
     private FirebaseAuth mAuth;
 
@@ -50,6 +51,17 @@ import com.google.firebase.auth.FirebaseAuth;
                                 if (task.isSuccessful()) {
                                     Toast.makeText(getApplicationContext(), "인증 이메일을 전송했습니다",
                                             Toast.LENGTH_SHORT).show();
+                                }
+                                else if(task.getException() instanceof FirebaseAuthInvalidUserException){
+                                    Toast.makeText(PasswordResetActivity.this, "해당하는 계정 정보가 없습니다",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                                else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "PasswordResetEmail:failure", task.getException());
+                                    Toast.makeText(PasswordResetActivity.this, "메일 전송에 실패했습니다",
+                                            Toast.LENGTH_SHORT).show();
+
                                 }
                             }
                         });
