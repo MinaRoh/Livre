9a6d8ff4c7320906c62382ef91841211f7441c34
Yumi Ko <kumi3994@gmail.com>
2021-05-19 01:24:13 +0900

    Update PostActivity
    업로드한 글에 프로필 사진 보이게 함

--- app/src/main/java/gachon/mp/livre_bottom_navigation/ui/writing/PostActivity.java
+++ app/src/main/java/gachon/mp/livre_bottom_navigation/ui/writing/PostActivity.java
@@ -24,6 +24,8 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
@@ -38,6 +40,7 @@ public class PostActivity extends AppCompatActivity {
     private static final String TAG = "PostActivity";
     private String posts_id;
     private FirebaseUser user;
+    private String profileImg = "";
     ImageView post_image;
     int int_num_heart;
     int int_num_comment;
@@ -96,6 +99,42 @@ public class PostActivity extends AppCompatActivity {
 
 
         });
+        /*유저 프로필 이미지 불러오기*/
+        FirebaseFirestore db_user = FirebaseFirestore.getInstance();
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        db_user.collection("Users")
+                .whereEqualTo("uid", user.getUid())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                profileImg = document.get("profileImage").toString();
+                            }
+                            //FirebaseStorage 인스턴스를 생성
+                            FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
+                            // 위의 저장소를 참조하는 파일명으로 지정
+                            StorageReference storageReference = firebaseStorage.getReferenceFromUrl("gs://mp-livre.appspot.com/"+profileImg);
+                            //StorageReference에서 파일 다운로드 URL 가져옴
+                            storageReference.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Uri> task) {
+                                    if (task.isSuccessful()) {
+                                        // Glide 이용하여 이미지뷰에 로딩
+                                        Glide.with(PostActivity.this)
+                                                .load(task.getResult())
+                                                .override(1024, 980)
+                                                .into(user_profile);
+                                    } else {
+                                        // URL을 가져오지 못하면 토스트 메세지
+                                        Toast.makeText(PostActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                        }
+                    }
+                });
         /*하트 눌렀을 때*/
         heart.setOnClickListener(new View.OnClickListener() {
             @Override
